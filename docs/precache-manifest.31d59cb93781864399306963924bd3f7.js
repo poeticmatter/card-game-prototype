02a967ebd7745f2eb502dee3df44fb4bf09765ee
Aurore Inara <d00m.poet@gmail.com>
2023-05-16 00:14:45 +0300

    build

---
+++ docs/precache-manifest.31d59cb93781864399306963924bd3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36a389bd57b26bdcb341",
+    "url": "/card-game-prototype/static/css/main.7e3f214b.chunk.css"
+  },
+  {
+    "revision": "36a389bd57b26bdcb341",
+    "url": "/card-game-prototype/static/js/main.36a389bd.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/card-game-prototype/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "358dedb88ff232acc1b4",
+    "url": "/card-game-prototype/static/js/2.358dedb8.chunk.js"
+  },
+  {
+    "revision": "e3abbdefad6bb2f79599",
+    "url": "/card-game-prototype/static/js/runtime~main.e3abbdef.js"
+  },
+  {
+    "revision": "785d4503ec23f00b765f661b54faf042",
+    "url": "/card-game-prototype/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.927df3fffbf0742f50c3713a9f787134.js"
+  "/card-game-prototype/precache-manifest.31d59cb93781864399306963924bd3f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/card-game-prototype/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
