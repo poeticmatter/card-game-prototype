6e9f592f0c3ee6c5cc8d2f7e6f68a11dba387054
Aurore Inara <d00m.poet@gmail.com>
2023-05-17 13:33:38 +0300

    Added cardColor

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,6 @@ const Card = ({ card, cardDisplayType }) => {
   const [isFaceUp, setIsFaceUp] = useState(
     cardDisplayType === "faceUp" || cardDisplayType === "both"
   );
-
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "card",
     item: { id: card.id },
@@ -20,12 +19,14 @@ const Card = ({ card, cardDisplayType }) => {
       setIsFaceUp(!isFaceUp);
     }
   };
-
   return (
     <div
       className={`card ${isFaceUp ? "" : "card-back"}`}
       ref={drag}
-      style={{ opacity: isDragging ? 0.5 : 1 }}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        backgroundColor: isFaceUp ? card.cardColor : "#f0f0f0", // Apply card color when face up
+      }}
       onClick={handleClick}
     >
       <h3 className="center-text">{isFaceUp ? card.cardName : ""}</h3>
